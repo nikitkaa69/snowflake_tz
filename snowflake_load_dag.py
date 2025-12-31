cf4e0a01df77beacd84321f0a656eb0411aa6b1d
Nikita <70703564+nikitkaa69@users.noreply.github.com>
2025-12-31 09:03:38 +0100

    fix: removed print with logging info in snowflake_load_dag.py

--- snowflake_load_dag.py
+++ snowflake_load_dag.py
@@ -27,27 +27,24 @@ def snowflake_load_dag():
     @task
     def upload_to_stage():
         run_snowflake_query(snowflake_sql.UPLOAD_TO_STAGE_SQL)
-        print(f"File {config.FILE_PATH} uploaded to stage successfully.")
 
     # Copy data from Stage to RAW table
     @task
     def copy_into_table():
         run_snowflake_query(snowflake_sql.COPY_INTO_TABLE_SQL)
-        print("Data copied from Stage to RAW table")
 
     # Transform RAW -> CORE (Stored Procedure)
     @task
     def call_core_procedure():
         run_snowflake_query(snowflake_sql.CALL_CORE_PROC_SQL)
-        print("Core layer procedure executed")
 
     # Transform CORE -> MARTS (Stored Procedure)
     @task
     def call_marts_procedure():
         run_snowflake_query(snowflake_sql.CALL_MARTS_PROC_SQL)
-        print("Marts layer procedure executed")
 
     upload_to_stage() >> copy_into_table() >> call_core_procedure() >> call_marts_procedure()
 
 
 dag_instance = snowflake_load_dag()
+
